0773cc059892b7a5be3ef8ed4471937dcfab039f
Norman Yan <hwyan0220@gmail.com>
2020-07-12 22:36:43 +1000

    updated server file

--- app/server.py
+++ app/server.py
@@ -17,9 +17,9 @@ path = Path(__file__).parent
 app = Starlette()
 app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
 
-app.mount('/static', StaticFiles(directory='static'))
+# app.mount('/static', StaticFiles(directory='static'))
 
-# app.mount('/static', StaticFiles(directory='app/static'))
+app.mount('/static', StaticFiles(directory='app/static'))
 
 async def download_file(url, dest):
     if dest.exists(): return
